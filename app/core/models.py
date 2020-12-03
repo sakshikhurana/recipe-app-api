216f00321339f0d16795778f867b51f70fb9d258
Sakshi Khurana <sakshikhurana888@gmail.com>
2020-12-03 21:44:36 +0530

    superuser code added

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and save new user"""
+        if not email:
+            raise ValueError("User must have email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None):
+        """Create and save new user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,17 @@ class ModelTestCase(TestCase):
         user = get_user_model().objects.create_user(
             email=email, password=password)
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test invalid email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test@123')
+
+    def test_create_superuser_with_email_successful(self):
+        """Test the user is created with email successfully"""
+        email = 'test@gmail.com'
+        password = 'password123'
+        user = get_user_model().objects.create_superuser(
+            email=email, password=password)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
